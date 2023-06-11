65996a272ebb86d7d71d530e54f0801c305fd1ad
Mohammad Sumer Ali <134462535+sumerx47@users.noreply.github.com>
2023-06-11 15:25:18 +0530

    Add files via upload

---
+++ 00_python_code.py
@@ -0,0 +1,382 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from feature_engine.outliers import Winsorizer
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.pipeline import Pipeline
+import joblib
+from sklearn.tree import DecisionTreeClassifier as DT
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report, roc_curve, roc_auc_score
+import sklearn.metrics as skmet
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+# Load the dataset
+data = pd.read_csv(r'C:\Users\sumair\OneDrive\Desktop\post liver transplantation prediction\dataset\LiverT_dataset.csv')
+
+
+# Define a dictionary mapping old column names to new column names
+column_mapping = {
+    'D Age': 'donor_age',
+    'D Gender': 'donor_gender',
+    'D BMI': 'donor_bmi',
+    'D Cause of Death': 'donor_cause_of_death',
+    'D Diabetes': 'donor_diabetes',
+    'D Hypertension': 'donor_hypertension',
+    'D Alcohol Abuse': 'donor_alcohol_abuse',
+    'D Smoking': 'donor_smoking',
+    'D Lympochyte': 'donor_lymphocyte',
+    'D Hepatitis B': 'donor_hepatitis_b',
+    'D Hepatitis C': 'donor_hepatitis_c',
+    'R Etiology': 'recipient_etiology',
+    'R MELD Score': 'recipient_meld_score',
+    'R Age': 'recipient_age',
+    'R Gender': 'recipient_gender',
+    'R BMI': 'recipient_bmi',
+    'R Diabetes': 'recipient_diabetes',
+    'R Hypertension': 'recipient_hypertension',
+    'R Alcohol Abuse': 'recipient_alcohol_abuse',
+    'R Smoking': 'recipient_smoking',
+    'R Lympochyte': 'recipient_lymphocyte',
+    'R Hepatitis B': 'recipient_hepatitis_b',
+    'R Hepatitis C': 'recipient_hepatitis_c',
+    'R Albumin level': 'recipient_albumin_level',
+    'R Alcoholic cirrhosis': 'recipient_alcoholic_cirrhosis',
+    'R Primary biliary cirrhosis': 'recipient_primary_biliary_cirrhosis',
+    'R Na': 'recipient_na',
+    'R Mg': 'recipient_mg',
+    'R WBC': 'recipient_wbc',
+    'R Platelets': 'recipient_platelets',
+    'R Cold Ischemia Time': 'recipient_cold_ischemia_time',
+    'R Warm Ischemia Time': 'recipient_warm_ischemia_time',
+    'R Blood Transfusion': 'recipient_blood_transfusion',
+    'R Immunosuppressant Medication': 'recipient_immunosuppressant_medication',
+    'R Rejection Episodes': 'recipient_rejection_episodes',
+    'Complications':'complications'
+}
+
+# Rename the columns using the column_mapping dictionary
+data = data.rename(columns=column_mapping)
+
+
+# Represents the information related to the dataset
+data.info()
+
+# Represents the total number of value each of the column has.
+data.count()
+
+# Represents the number of elements present in each row and total number of columns
+data.shape
+
+# Represents the each column name
+data.columns
+
+# Represents the first 5 rows with all columns of the data frame
+data.head(5)
+
+# Represents the last 5 rows with all columns of the data frame
+data.tail(5)
+
+# Dropping Time column
+df = data.drop(['Column1'], axis=1)
+
+
+## Exploratory Data Analysis (EDA)
+# First Business Moment
+# Mean
+df.mean()
+
+# Median
+df.median()
+
+# Mode
+df.mode()
+
+# Second Business Moment
+# Standard Deviation
+df.std()
+
+# Variance
+df.var()
+
+# Third Business Moment
+# Skewness
+df.skew()
+
+# Fourth Business Moment
+# Kurtosis
+df.kurt()
+
+df.dtypes
+
+# Univariate Plot (Histogram) for each column
+for column in df.columns:
+    plt.figure()
+    df[column].hist()
+    plt.xlabel(column)
+    plt.ylabel('Frequency')
+    plt.title(f'Histogram of {column}')
+
+# Density Plot for each column
+numeric_columns = ['donor_age', 'donor_bmi', 'recipient_meld_score', 'recipient_age', 'recipient_bmi',
+                   'recipient_albumin_level', 'recipient_na', 'recipient_mg', 'recipient_wbc', 'recipient_platelets',
+                   'recipient_cold_ischemia_time', 'recipient_warm_ischemia_time', 'recipient_rejection_episodes']
+
+for column in numeric_columns:
+    plt.figure()
+    sns.kdeplot(df[column], shade=True)
+    plt.xlabel(column)
+    plt.ylabel('Density')
+    plt.title(f'Density Plot of {column}')
+plt.show()
+
+
+# Boxplot for numeric columns
+numeric_columns = ['donor_age', 'donor_bmi', 'recipient_meld_score', 'recipient_age', 'recipient_bmi',
+                   'recipient_albumin_level', 'recipient_na', 'recipient_mg', 'recipient_wbc', 'recipient_platelets','recipient_alcoholic_cirrhosis', 'recipient_primary_biliary_cirrhosis',
+                   'recipient_cold_ischemia_time', 'recipient_warm_ischemia_time', 'recipient_rejection_episodes']
+
+for column in numeric_columns:
+    plt.figure()
+    sns.boxplot(data=df[column])
+    plt.xlabel(column)
+    plt.title(f'Boxplot of {column}')
+
+plt.show()
+
+# Boxplot for categorical columns
+categorical_columns = ['donor_gender', 'donor_cause_of_death', 'donor_diabetes', 'donor_hypertension',
+                       'donor_alcohol_abuse', 'donor_smoking', 'donor_lymphocyte', 'donor_hepatitis_b',
+                       'donor_hepatitis_c', 'recipient_etiology', 'recipient_gender', 'recipient_diabetes',
+                       'recipient_hypertension', 'recipient_alcohol_abuse', 'recipient_smoking',
+                       'recipient_lymphocyte', 'recipient_hepatitis_b', 'recipient_hepatitis_c',
+                       
+                       'recipient_blood_transfusion', 'recipient_immunosuppressant_medication']
+
+for column in categorical_columns:
+    plt.figure()
+    sns.boxplot(data=df, x=column, y='recipient_age', hue=column)
+    plt.ylabel('recipient_age')
+    plt.title(f'Boxplot of recipient_age by {column}')
+
+plt.show()
+
+
+# Count Plot for each column
+for column in df.columns:
+    plt.figure()
+    sns.countplot(data[column])
+    plt.xlabel(column)
+    plt.ylabel('Count')
+    plt.title(f'Count Plot of {column}')
+
+
+# Scatter Plot for numeric columns
+numeric_columns = ['donor_age', 'donor_bmi', 'recipient_meld_score', 'recipient_age', 'recipient_bmi',
+                   'recipient_albumin_level', 'recipient_na', 'recipient_mg', 'recipient_wbc', 'recipient_platelets',
+                   'recipient_cold_ischemia_time', 'recipient_warm_ischemia_time', 'recipient_rejection_episodes']
+for column in numeric_columns:
+    plt.figure()
+    plt.scatter(data[column], data['complications'])
+    plt.xlabel(column)
+    plt.ylabel('Complications')
+    plt.title(f'Scatter Plot of {column} vs Complications')
+
+# Bar Plot for categorical columns
+categorical_columns = ['donor_gender', 'donor_cause_of_death', 'donor_diabetes', 'donor_hypertension',
+                       'donor_alcohol_abuse', 'donor_smoking', 'donor_lymphocyte', 'donor_hepatitis_b',
+                       'donor_hepatitis_c', 'recipient_etiology', 'recipient_gender', 'recipient_diabetes',
+                       'recipient_hypertension', 'recipient_alcohol_abuse', 'recipient_smoking',
+                       'recipient_lymphocyte', 'recipient_hepatitis_b', 'recipient_hepatitis_c',
+                       'recipient_alcoholic_cirrhosis', 'recipient_primary_biliary_cirrhosis',
+                       'recipient_blood_transfusion', 'recipient_immunosuppressant_medication']
+for column in categorical_columns:
+    plt.figure()
+    sns.countplot(data[column], hue=data['complications'])
+    plt.xlabel(column)
+    plt.ylabel('Count')
+    plt.title(f'Count Plot of {column} with Complications')
+
+# Heatmap of correlation matrix
+correlation_matrix = data.corr()
+plt.figure(figsize=(12, 10))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm')
+plt.title('Correlation Matrix')
+
+# Pairwise scatter plot of numeric columns
+sns.pairplot(data[numeric_columns], diag_kind='kde')
+plt.show()
+
+
+
+# Univariate Plot
+# Pie chart to check the ratio of data 
+data["Complications"].value_counts().plot.pie(autopct = '%.1f')
+
+#### Bi-variate plot
+# Creating heatmap
+plt.figure(figsize=(15,10))
+sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
+
+###  Multi variate Plot
+# Pair plot between the variables  
+sns.pairplot(df)
+
+# Correlation matrix 
+df.corr()
+
+
+
+# Check for missing/Null values
+df.isnull().sum()
+
+# Duplicates
+df.duplicated().sum()
+df1 = df.drop_duplicates()
+df1.duplicated().sum()
+
+
+# Handle missing values
+imputer = SimpleImputer(strategy='median')
+numerical_cols = df.select_dtypes(include=np.number).columns
+categorical_cols = df.select_dtypes(exclude=np.number).columns
+
+df[numerical_cols] = imputer.fit_transform(df[numerical_cols])
+
+for col in categorical_cols:
+    df[col].fillna(df[col].mode()[0], inplace=True)
+
+
+X = df.drop('complications', axis=1)
+Y = df['complications']
+    
+# Perform label encoding for categorical columns
+label_encoder = LabelEncoder()
+for column in X.select_dtypes(include='object').columns:
+    X[column] = label_encoder.fit_transform(X[column])
+Y = label_encoder.fit_transform(Y)
+
+
+# Split the data into train and test sets
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, stratify=Y, random_state=42)
+
+# Define a dictionary to store the model accuracies
+model_accuracies = {}
+
+
+# Train and evaluate Decision Tree Classifier
+dt_classifier = DecisionTreeClassifier()
+dt_classifier.fit(X_train, Y_train)
+dt_pred = dt_classifier.predict(X_test)
+dt_accuracy = accuracy_score(Y_test, dt_pred)
+model_accuracies['Decision Tree'] = dt_accuracy
+
+# Train and evaluate Logistic Regression
+lr_classifier = LogisticRegression()
+lr_classifier.fit(X_train, Y_train)
+lr_pred = lr_classifier.predict(X_test)
+lr_accuracy = accuracy_score(Y_test, lr_pred)
+model_accuracies['Logistic Regression'] = lr_accuracy
+
+# Train and evaluate Random Forest Classifier
+rf_classifier = RandomForestClassifier()
+rf_classifier.fit(X_train, Y_train)
+rf_pred = rf_classifier.predict(X_test)
+rf_accuracy = accuracy_score(Y_test, rf_pred)
+model_accuracies['Random Forest'] = rf_accuracy
+
+# Train and evaluate Support Vector Classifier
+svm_classifier = SVC()
+svm_classifier.fit(X_train, Y_train)
+svm_pred = svm_classifier.predict(X_test)
+svm_accuracy = accuracy_score(Y_test, svm_pred)
+model_accuracies['Support Vector Machine'] = svm_accuracy
+
+
+#################    Blackbox techniques , Emsembled techniques
+
+#pip install xgboost
+#pip install lightgbm
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from xgboost import XGBClassifier
+#from lightgbm import LGBMClassifier
+from sklearn.ensemble import VotingClassifier
+
+#Train and evaluate additional classifiers:
+#Multilayer Perceptron (Neural Network) Classifier:
+mlp_classifier = MLPClassifier()
+mlp_classifier.fit(X_train, Y_train)
+mlp_pred = mlp_classifier.predict(X_test)
+mlp_accuracy = accuracy_score(Y_test, mlp_pred)
+model_accuracies['Multilayer Perceptron'] = mlp_accuracy
+
+
+#Apply ensemble techniques:
+#AdaBoost Classifier:
+ada_classifier = AdaBoostClassifier()
+ada_classifier.fit(X_train, Y_train)
+ada_pred = ada_classifier.predict(X_test)
+ada_accuracy = accuracy_score(Y_test, ada_pred)
+model_accuracies['AdaBoost'] = ada_accuracy
+
+#Gradient Boosting Classifier:
+gb_classifier = GradientBoostingClassifier()
+gb_classifier.fit(X_train, Y_train)
+gb_pred = gb_classifier.predict(X_test)
+gb_accuracy = accuracy_score(Y_test, gb_pred)
+model_accuracies['Gradient Boosting'] = gb_accuracy
+
+
+#XGBoost Classifier:
+label_encoder = LabelEncoder()
+y_encoded = label_encoder.fit_transform(Y)
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, y_encoded, test_size=0.2, random_state=0)
+# Rest of the code for XGBoost Classifier
+classifier = XGBClassifier()
+classifier.fit(X_train, y_train)
+xgb_pred = classifier.predict(X_test)
+xgb_accuracy = accuracy_score(y_test, xgb_pred)
+model_accuracies['XGBoost'] = xgb_accuracy
+
+
+#Voting Classifier (Combining multiple models):
+voting_classifier = VotingClassifier(
+    estimators=[('dt', dt_classifier), ('lr', lr_classifier), ('rf', rf_classifier)],
+    voting='hard'
+)
+voting_classifier.fit(X_train, y_train)
+voting_pred = voting_classifier.predict(X_test)
+voting_accuracy = accuracy_score(y_test, voting_pred)
+model_accuracies['Voting Classifier'] = voting_accuracy
+
+
+#Print the accuracies of the trained models:
+for model, accuracy in model_accuracies.items():
+    print(f"{model} Accuracy: {accuracy}")
+
+
+
+
+
+import pickle
+
+pickle_out=open("gb_classifier.pkl","wb")
+pickle.dump(gb_classifier,pickle_out)
+pickle_out.close()
+
+
+
+
